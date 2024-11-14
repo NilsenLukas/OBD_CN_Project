99abf45ce1cda21d5af72c6ba74686572329fb4d
nilsenlukas <lukas.nilsen2021lax@gmail.com>
2024-11-14 17:48:37 -0500

    first test code

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.json());
+
+// Endpoint for receiving CAN data from Raspberry Pi
+app.post('/update_data', (req, res) => {
+    const canData = req.body;
+    console.log('Received CAN data:', canData);
+
+    // Broadcast CAN data to all connected clients
+    io.emit('can_data', canData);
+    res.sendStatus(200);
+});
+
+// Serve a simple HTML file (for testing display)
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3232, () => {
+    console.log('Server running on port 3232');
+});
